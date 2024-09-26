e176a48f5c7a7b2d79695ab559023e0c078e04b5
fylora <fylora189@gmail.com>
2024-09-26 22:11:31 +0300

    Edited doc card

--- composeApp/src/desktopMain/kotlin/org/dvir/docsync/core/ui/Color.kt
+++ composeApp/src/desktopMain/kotlin/org/dvir/docsync/core/ui/Color.kt
@@ -2,8 +2,9 @@ package org.dvir.docsync.core.ui
 
 import androidx.compose.ui.graphics.Color
 
-val PrimaryColor = Color(0xFF3E82F7)
+val PrimaryColor = Color(0xFF4084FC)
+val SecondaryColor = Color(0xFFD0E1FF)
 val BackgroundColor = Color(0xFFFFFFFF)
 val TextColor = Color(0xFF212121)
-val SurfaceColor = Color(0xFFEEEEEE)
+val SurfaceColor = Color(0xFFF0F0F0)
 val ErrorColor = Color.Red
--- composeApp/src/desktopMain/kotlin/org/dvir/docsync/doc/presentation/home/HomeScreen.kt
+++ composeApp/src/desktopMain/kotlin/org/dvir/docsync/doc/presentation/home/HomeScreen.kt
@@ -6,12 +6,22 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.dvir.docsync.core.navigation.Route
+import org.dvir.docsync.core.ui.BackgroundColor
+import org.dvir.docsync.core.ui.PrimaryColor
+import org.dvir.docsync.core.ui.SecondaryColor
+import org.dvir.docsync.core.ui.TextColor
 import org.dvir.docsync.doc.domain.model.Document
 import org.dvir.docsync.doc.presentation.home.viewmodel.HomeEvent
 import org.dvir.docsync.doc.presentation.home.viewmodel.HomeViewModel
@@ -25,80 +35,126 @@ fun HomeScreen(
     viewmodel: HomeViewModel,
     onNavigateToDoc: (Route.Doc) -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "Recent documents",
-            style = MaterialTheme.typography.h6,
-            modifier = Modifier.padding(vertical = 16.dp)
-        )
-
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(200.dp),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.weight(1f)
-        ) {
-            items(viewmodel.docs.value) { document ->
-                DocumentCard(document, viewmodel)
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                shape = CircleShape,
+                backgroundColor = PrimaryColor,
+                onClick = {
+                    viewmodel.onEvent(
+                        HomeEvent.OpenCreateDialog
+                    )
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add doc",
+                    tint = BackgroundColor
+                )
             }
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                viewmodel.onEvent(
-                    HomeEvent.OpenCreateDialog
-                )
-            },
-            modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            Text("Create New Document")
-        }
-    }
+            Text(
+                text = "Recent documents",
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
 
-    if (viewmodel.isCreateDialogOpened.value) {
-        CreateDocDialog(
-            viewModel = viewmodel,
-            onDismiss = { viewmodel.onEvent(HomeEvent.OpenCreateDialog) },
-            onCreate = { viewmodel.onEvent(HomeEvent.CreateDoc) }
-        )
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(200.dp),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.weight(1f)
+            ) {
+                items(viewmodel.docs.value) { document ->
+                    DocumentCard(
+                        document,
+                        onDelete = {
+                            viewmodel.onEvent(
+                                HomeEvent.DeleteDoc(document.id)
+                            )
+                        },
+                        onClick = {
+                            viewmodel.onEvent(
+                                HomeEvent.GetDoc(document.id)
+                            )
+                        }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
+        if (viewmodel.isCreateDialogOpened.value) {
+            CreateDocDialog(
+                viewModel = viewmodel,
+                onDismiss = { viewmodel.onEvent(HomeEvent.OpenCreateDialog) },
+                onCreate = { viewmodel.onEvent(HomeEvent.CreateDoc) }
+            )
+        }
     }
 }
 
 @Composable
-fun DocumentCard(document: Document, viewModel: HomeViewModel) {
+fun DocumentCard(
+    document: Document,
+    onDelete: () -> Unit,
+    onClick: () -> Unit
+) {
     Column(
         modifier = Modifier
-            .background(MaterialTheme.colors.surface, RoundedCornerShape(8.dp))
-            .clickable {
-                viewModel.onEvent(HomeEvent.GetDoc(document.id))
-            }
+            .background(SecondaryColor)
+            .clickable(
+                onClick = onClick
+            )
             .padding(16.dp)
     ) {
-        Text(text = document.name, style = MaterialTheme.typography.h6)
+        Column(
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = document.name,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold,
+                )
+                Spacer(modifier = Modifier.width(40.dp))
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = "Delete",
+                    tint = TextColor,
+                    modifier = Modifier.clickable {
+                        onDelete()
+                    }
+                )
+            }
 
-        Text(
-            text = "Owner: ${document.owner}",
-            style = MaterialTheme.typography.body2,
-            modifier = Modifier.padding(top = 8.dp)
-        )
+            Text(
+                text = "Owner: ${document.owner}",
+                fontSize = 16.sp,
+                modifier = Modifier.padding(top = 16.dp)
+            )
 
-        val creationDate = LocalDateTime.ofInstant(
-            Instant.ofEpochMilli(document.creationDate),
-            ZoneId.systemDefault()
-        ).format(DateTimeFormatter.ofPattern("MMM dd, yyyy"))
+            val creationDate = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(document.creationDate),
+                ZoneId.systemDefault()
+            ).format(DateTimeFormatter.ofPattern("MMM dd, yyyy"))
 
-        Text(
-            text = "Created on: $creationDate",
-            style = MaterialTheme.typography.body2,
-            modifier = Modifier.padding(top = 8.dp)
-        )
+            Text(
+                text = creationDate,
+                fontSize = 16.sp,
+            )
+        }
     }
 }
 
